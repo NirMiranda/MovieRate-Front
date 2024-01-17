115e80a4aa00d230bcdeffc474daa476d000fe03
ilaycohen2712 <ilaycohen1998@gmail.com>
2024-01-17 18:49:41 +0200

    code clean

--- src/pages/MoviesPage/MoviesPage.tsx
+++ src/pages/MoviesPage/MoviesPage.tsx
@@ -14,13 +14,14 @@ export type movie = {
   reviews?: string[],
   trailer: string
 }
+
 function Movies() {
   const [movies, setMovies] = useState<movie[]>([]);
   const [moviestopten, setMoviestopten] = useState<movie[]>([]);
 
   const [startIndextop, setStartIndextop] = useState(0);
   const [startIndexAll, setStartIndexAll] = useState(0);
-  const moviesPerPage = 4; // Number of movies to display at a time
+  const moviesPerPage = 4;
 
   const getaAllMovies = async () => {
     try {
@@ -28,25 +29,23 @@ function Movies() {
       setMovies(data);
     } catch (error) { console.error("Error fetching movies:", error); }
   };
+
   const getTop10Movies = async () => {
     try {
       const { data } = await axios.get("http://localhost:3003/movie/getAllMovies");
-      // Sort movies by IMDb rating in descending order
       const sortedMovies = data.sort((a: { ratingImdb: number; }, b: { ratingImdb: number; }) => b.ratingImdb - a.ratingImdb);
-      // Take only the top 10 movies
       const top10Movies = sortedMovies.slice(0, 10);
       setMoviestopten(top10Movies);
-    } catch (error) {
-      console.error("Error fetching movies:", error);
-    }
+    } catch (error) { console.error("Error fetching movies:", error); }
   };
+
   const handleRightNarrowClickTop = () => {
     setStartIndextop((prevIndex) => (prevIndex + moviesPerPage) % movies.length);
   };
 
   const handleLeftNarrowClickTop = () => {
-    setStartIndextop(
-      (prevIndex) => (prevIndex - moviesPerPage + movies.length) % movies.length);
+    setStartIndextop((prevIndex) => (prevIndex - moviesPerPage + movies.length) %
+      movies.length);
   };
 
   const handleRightNarrowClickAll = () => {
@@ -54,10 +53,9 @@ function Movies() {
   };
 
   const handleLeftNarrowClickAll = () => {
-    setStartIndexAll(
-      (prevIndex) => (prevIndex - moviesPerPage + movies.length) % movies.length);
+    setStartIndexAll((prevIndex) => (prevIndex - moviesPerPage + movies.length) %
+      movies.length);
   };
-
   useEffect(() => { getaAllMovies(); }, []);
   useEffect(() => { getTop10Movies(); }, []);
 
@@ -70,9 +68,9 @@ function Movies() {
           </h2>
         </div>
         <div style={{ display: 'flex', justifyContent: 'center', overflow: 'hidden' }}>
-          <div className="Leftnarrow-topten" role="presentation" onClick={handleLeftNarrowClickTop}>
-            <svg
-              width="60" height="60" color="white"
+          <div className="Leftnarrow-topten" role="presentation"
+            onClick={handleLeftNarrowClickTop}>
+            <svg width="60" height="60" color="white"
               xmlns="http://www.w3.org/2000/svg"
               className="ipc-icon ipc-icon--chevron-left-inline ipc-icon--inline ipc-pager-icon"
               viewBox="0 0 24 24"
@@ -82,12 +80,9 @@ function Movies() {
               <path d="M18.378 23.369c.398-.402.622-.947.622-1.516 0-.568-.224-1.113-.622-1.515l-8.249-8.34 8.25-8.34a2.16 2.16 0 0 0 .548-2.07A2.132 2.132 0 0 0 17.428.073a2.104 2.104 0 0 0-2.048.555l-9.758 9.866A2.153 2.153 0 0 0 5 12.009c0 .568.224 1.114.622 1.515l9.758 9.866c.808.817 2.17.817 2.998-.021z"></path>
             </svg>
           </div>
-          {moviestopten && moviestopten.slice(startIndextop, startIndextop + moviesPerPage).map((movie) => (
-            <Moviecard key={movie._id} data={movie} style={{ margin: '0 10px' }} />
-          ))}
+          {moviestopten && moviestopten.slice(startIndextop, startIndextop + moviesPerPage).map((movie) => (<Moviecard key={movie._id} data={movie} style={{ margin: '0 10px' }} />))}
           <div className="Rightnarrow-topten" role="presentation" onClick={handleRightNarrowClickTop}>
-            <svg
-              width="60" height="60" color="white"
+            <svg width="60" height="60" color="white"
               xmlns="http://www.w3.org/2000/svg"
               className="ipc-icon ipc-icon--chevron-right-inline ipc-icon--inline ipc-pager-icon"
               viewBox="0 0 24 24"
@@ -102,16 +97,9 @@ function Movies() {
       <section className="all-movies" style={{ marginBottom: '50px', height: '500px' }}>
         <div style={{ marginLeft: '10px', display: 'flex', alignItems: 'center' }}>
           <h2 style={{ color: 'white', margin: '0 10px', marginBottom: '20px' }}>
-            All movies:
-          </h2>
+            All movies:</h2>
         </div>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            overflow: 'hidden',
-            marginBottom: '10px',
-          }}>
+        <div style={{ display: 'flex', justifyContent: 'center', overflow: 'hidden', marginBottom: '10px', }}>
           <div className="Leftnarrow-allmovies" role="presentation" onClick={handleLeftNarrowClickAll}>
             <svg width="60" height="60" color="white"
               xmlns="http://www.w3.org/2000/svg"
@@ -123,15 +111,10 @@ function Movies() {
               <path d="M18.378 23.369c.398-.402.622-.947.622-1.516 0-.568-.224-1.113-.622-1.515l-8.249-8.34 8.25-8.34a2.16 2.16 0 0 0 .548-2.07A2.132 2.132 0 0 0 17.428.073a2.104 2.104 0 0 0-2.048.555l-9.758 9.866A2.153 2.153 0 0 0 5 12.009c0 .568.224 1.114.622 1.515l9.758 9.866c.808.817 2.17.817 2.998-.021z"></path>
             </svg>
           </div>
-          {movies && movies.slice(startIndexAll, startIndexAll + moviesPerPage).map((movie) => (
-            <Moviecard key={movie._id} data={movie} style={{ margin: '0 10px' }} />
-          ))}
+          {movies && movies.slice(startIndexAll, startIndexAll + moviesPerPage).map((movie) => (<Moviecard key={movie._id} data={movie} style={{ margin: '0 10px' }} />))}
           <div className="Rightnarrow-allmovies" role="presentation" onClick={handleRightNarrowClickAll}>
-            <svg
-              width="60"
-              height="60"
+            <svg width="60" height="60" color="white"
               xmlns="http://www.w3.org/2000/svg"
-              color="white"
               className="ipc-icon ipc-icon--chevron-right-inline ipc-icon--inline ipc-pager-icon"
               viewBox="0 0 24 24"
               fill="currentColor"
