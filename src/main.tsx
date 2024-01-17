cc83e2abf506a85f680ab373473db34d9ad876d3
ilaycohen2712 <ilaycohen1998@gmail.com>
2024-01-17 14:17:36 +0200

    adding backgrounnd and display the items

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,8 @@ import App from './App.tsx'
 
 
 
-const rootElement = document.getElementById('root');
-rootElement && rootElement.setAttribute("style", "background-color: black; hight: 100%")
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-
   <App />
 )
