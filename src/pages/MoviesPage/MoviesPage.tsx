9be984d0e15ce1dc4ab70980bb1ba5b00db725d6
ilaycohen2712 <ilaycohen1998@gmail.com>
2024-01-17 18:16:29 +0200

    sorting the top10 according to imdb and display only the first ten rating

--- src/pages/MoviesPage/MoviesPage.tsx
+++ src/pages/MoviesPage/MoviesPage.tsx
@@ -16,6 +16,8 @@ export type movie = {
 }
 function Movies() {
   const [movies, setMovies] = useState<movie[]>([]);
+  const [moviestopten, setMoviestopten] = useState<movie[]>([]);
+
   const [startIndextop, setStartIndextop] = useState(0);
   const [startIndexAll, setStartIndexAll] = useState(0);
   const moviesPerPage = 4; // Number of movies to display at a time
@@ -26,7 +28,18 @@ function Movies() {
       setMovies(data);
     } catch (error) { console.error("Error fetching movies:", error); }
   };
-
+  const getTop10Movies = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:3003/movie/getAllMovies");
+      // Sort movies by IMDb rating in descending order
+      const sortedMovies = data.sort((a: { ratingImdb: number; }, b: { ratingImdb: number; }) => b.ratingImdb - a.ratingImdb);
+      // Take only the top 10 movies
+      const top10Movies = sortedMovies.slice(0, 10);
+      setMoviestopten(top10Movies);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
+  };
   const handleRightNarrowClickTop = () => {
     setStartIndextop((prevIndex) => (prevIndex + moviesPerPage) % movies.length);
   };
@@ -46,6 +59,8 @@ function Movies() {
   };
 
   useEffect(() => { getaAllMovies(); }, []);
+  useEffect(() => { getTop10Movies(); }, []);
+
   return (
     <div>
       <section className="top-ten" style={{ height: '500px' }}>
@@ -67,7 +82,7 @@ function Movies() {
               <path d="M18.378 23.369c.398-.402.622-.947.622-1.516 0-.568-.224-1.113-.622-1.515l-8.249-8.34 8.25-8.34a2.16 2.16 0 0 0 .548-2.07A2.132 2.132 0 0 0 17.428.073a2.104 2.104 0 0 0-2.048.555l-9.758 9.866A2.153 2.153 0 0 0 5 12.009c0 .568.224 1.114.622 1.515l9.758 9.866c.808.817 2.17.817 2.998-.021z"></path>
             </svg>
           </div>
-          {movies && movies.slice(startIndextop, startIndextop + moviesPerPage).map((movie) => (
+          {moviestopten && moviestopten.slice(startIndextop, startIndextop + moviesPerPage).map((movie) => (
             <Moviecard key={movie._id} data={movie} style={{ margin: '0 10px' }} />
           ))}
           <div className="Rightnarrow-topten" role="presentation" onClick={handleRightNarrowClickTop}>
