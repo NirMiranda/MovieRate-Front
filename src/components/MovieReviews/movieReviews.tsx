19e1908eef2bca088e39a76e6c1e8f6f9f898168
Nir Miranda <nirmiranda2@gmail.com>
2024-01-20 06:22:16 +0200

    adding more things to movie page

--- src/components/MovieReviews/movieReviews.tsx
+++ src/components/MovieReviews/movieReviews.tsx
@@ -6,6 +6,7 @@ import { movie } from '../../pages/MoviesPage/MoviesPage';
 import CircularProgress from '@mui/joy/CircularProgress';
 import ReviewModal from '../reviewModal/reviewModal';
 import UnclickableLikeComponent  from '../unclickableLike/unclickableLike';
+import Button from '@mui/material/Button';
 
 
 
@@ -34,6 +35,9 @@ interface MovieReviewsProps {
   reviews: ReviewType[];
 }
 
+const REVIEWS_PER_ROW = 3;
+const MAX_ROWS = 3;
+
 function fixDate(date: Date) {
   const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
   const dateString = date.toLocaleDateString('en-US', options);
@@ -43,6 +47,13 @@ function fixDate(date: Date) {
 }
 
 function MovieReviews({ reviews }: MovieReviewsProps) {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const reviewsPerPage = REVIEWS_PER_ROW * MAX_ROWS;
+  const startIndex = (currentPage - 1) * reviewsPerPage;
+  const endIndex = Math.min(startIndex + reviewsPerPage, reviews.length);
+  const paginatedReviews = reviews.slice(startIndex, endIndex);
+
   const [hoveredReview, setHoveredReview] = useState<string | null>(null);
   const [openModal, setOpenModal] = useState<boolean>(false);
   const [selectedReviewId, setSelectedReviewId] = useState<string | null>(null);
@@ -66,42 +77,66 @@ function MovieReviews({ reviews }: MovieReviewsProps) {
   };
 
   const selectedReview = reviews.find((review) => review._id === selectedReviewId) || null;
+  console.log(paginatedReviews);
 
   return (
     <div className='reviewsBox'>
-      {reviews.map((review) => (
-        <div
-          key={review._id}
-          className={`ReviewItem`}
-          onMouseEnter={() => handleMouseEnter(review._id)}
-          onMouseLeave={handleMouseLeave}
-        >
-          <div className={`ContentContainer ${hoveredReview === review._id ? 'blurred' : ''}`}>
-            <Box className="Box">
-              <div className='messageIcon'><MessageOutlinedIcon style={{ color: 'black'}} /></div>
-              <div className='reviewDetails'>
-                <div className='reviewer'>
-                  Review by:<div className='reviwerName'>{review.reviewerId.name}</div>
+      <div className='reviewItems'>
+        {paginatedReviews.map((review) => (
+          <div
+            key={review._id}
+            className={`ReviewItem`}
+            onMouseEnter={() => handleMouseEnter(review._id)}
+            onMouseLeave={handleMouseLeave}
+          >
+            <div className={`ContentContainer ${hoveredReview === review._id ? 'blurred' : ''}`}>
+              <Box className="Box">
+                <div className='messageIcon'><MessageOutlinedIcon style={{ color: 'black'}} /></div>
+                <div className='reviewDetails'>
+                  <div className='reviewer'>
+                    Review by:<div className='reviwerName'>{review.reviewerId.name}</div>
+                  </div>
+                  <div className='reviewRating'>
+                    Score: <CircularProgress size="lg" determinate value={review.rating*10}>{review.rating} / 10</CircularProgress>
+                  </div>
+                  <div className='reviewBottom'>
+                    <div className='reviewDate'>{fixDate(new Date(review.date))}</div>
+                    <div className='unclickableLike'><UnclickableLikeComponent likes={review.likes ?? 0}/></div>
+                  </div>
                 </div>
-                <div className='reviewRating'>
-                  Score: <CircularProgress size="lg" determinate value={review.rating*10}>{review.rating} / 10</CircularProgress>
-                </div>
-                <div className='reviewBottom'>
-                <div className='reviewDate'>{fixDate(new Date(review.date))}</div>
-                  <div className='unclickableLike'><UnclickableLikeComponent  likes={review.likes ?? 0}/></div>
-                </div>
-                
-              </div>
-            </Box>
-          </div>
-          {hoveredReview === review._id && (
-            <div className={`expandText ${selectedReviewId === review._id ? 'clickable' : ''}`}
-              onClick={() => handleExpandClick(review._id)}>
-              Expand
+              </Box>
             </div>
-          )}
+            {hoveredReview === review._id && (
+              <div className={`expandText ${selectedReviewId === review._id ? 'clickable' : ''}`}
+                onClick={() => handleExpandClick(review._id)}>
+                Expand
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+
+      {reviews.length > reviewsPerPage && (
+        <div style={{ marginTop: '10px', textAlign: 'center' }}>
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={() => setCurrentPage(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Previous Page
+          </Button>
+          <span style={{ margin: '0 10px' }}>Page {currentPage}</span>
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={() => setCurrentPage(currentPage + 1)}
+            disabled={endIndex >= reviews.length}
+          >
+            Next Page
+          </Button>
         </div>
-      ))}
+      )}
 
       {/* Use the ReviewModal component */}
       <ReviewModal open={openModal} handleClose={handleCloseModal} review={selectedReview} />
--- src/pages/MoviePage/MoviePage.tsx
+++ src/pages/MoviePage/MoviePage.tsx
@@ -2,22 +2,27 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from "react-router-dom";
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import MovieLogo from '../../components/movieLogo/MovieLogo';
 import { movie } from '../MoviesPage/MoviesPage';
 import YouTube from 'react-youtube';
 import './MoviePage.css';
 import OtherInfo from '../../components/otherInfo/otherInfo';
 import MovieReviews,{ReviewType} from '../../components/movieReviews/movieReviews';
+import AddReviewModal from '../../components/addReviewModal/addReviewModal';
+
 
 
 
 
 function MoviePage() {
-  
+  const [isAddReviewModalOpen, setAddReviewModalOpen] = useState<boolean>(false);
   const [movieItem, setMovie] = useState<movie | undefined>(undefined);
   const movieId = useParams().id;
   const apiUrl = `http://localhost:3003/movie/getMovieById/${movieId}`;
+  const handleAddReviewClick = () => {
+    setAddReviewModalOpen(true);
+  };
 
 
   const fetchMovie = async () => {
@@ -29,6 +34,15 @@ function MoviePage() {
     } catch (error) {
       console.log(movieId);
       console.error("Error fetching movie:", error);
+    }
+  };
+
+  const refetchMovie = async () => {
+    try {
+      const response = await axios.get(apiUrl);
+      setMovie(response.data);
+    } catch (error) {
+      console.error("Error refetching movie:", error);
     }
   };
 
@@ -58,13 +72,23 @@ function MoviePage() {
           />
       </Box>
       </div>
-      <div className='reviews' style={{justifyContent:'center'}}> 
-         <h1 className='reviewsTitle' style={{textAlign:'center'}}>User Reviews</h1>
-          <MovieReviews reviews={movieItem?.reviews ?? []}/>
-
+      <div className='reviews' style={{ justifyContent: 'center' }}>
+        <h1 className='reviewsTitle' style={{ textAlign: 'center', marginTop: '20px' }}>Users Thoughts</h1>
+        <MovieReviews reviews={movieItem?.reviews ?? []} />
+        <div className='addReview'>
+          <Button variant="contained" onClick={handleAddReviewClick}>
+            Add your review
+          </Button>
+        </div>
       </div>
-    </div>
-    );
+      <AddReviewModal
+      open={isAddReviewModalOpen}
+      handleClose={() => setAddReviewModalOpen(false)}
+      movieItem={movieItem}
+      refetchMovie={refetchMovie}
+    />
+      </div>
+      );
 }
 
 
